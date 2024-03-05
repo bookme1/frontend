99e57f7365a02e6049dc80ae224b09fc99ab6169
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-03-05 22:28:33 +0200

    menu fix

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,68 +1,14 @@
 "use client";
 import { BreadCrumbs } from "@/components/common/BreadCrumbs";
-
-import {
-  WrapperStyle,
-  UserDiv,
-  UserImg,
-  UserName,
-  NavDiv,
-  Navli,
-  ExitDiv,
-  SpanStyle,
-} from "./page.style";
-import AvatarPhoto from "@/assets/account/UserAvatar.png";
 import { Header } from "@/components/common/Header";
-import { Icon } from "@/components/common/Icon";
-import { signOut } from "next-auth/react";
+import { LeftMenu } from "@/components/account/LeftMenu";
 
 export default function Home() {
   return (
     <>
       <Header />
       <BreadCrumbs name="акаунт" />
-      <WrapperStyle>
-        <UserDiv>
-          <UserImg src={AvatarPhoto.src} alt="" />
-          <UserName>Batman</UserName>
-        </UserDiv>
-        <NavDiv>
-          <ul>
-            <Navli>
-              <SpanStyle>
-                <Icon name="clock" />
-                Мої покупки
-              </SpanStyle>
-            </Navli>
-
-            <Navli>
-              <SpanStyle>
-                <Icon name="heart" />
-                Обране
-              </SpanStyle>
-            </Navli>
-            <Navli>
-              <SpanStyle>
-                <Icon name="wallet" /> Мій гаманець{" "}
-              </SpanStyle>
-            </Navli>
-          </ul>
-        </NavDiv>
-        <ExitDiv>
-          <Navli>
-            <button
-              onClick={() => {
-                signOut();
-                localStorage.removeItem("refreshToken");
-                localStorage.removeItem("accessToken");
-              }}
-            >
-              <Icon name="exit" />
-              Вийти
-            </button>
-          </Navli>
-        </ExitDiv>
-      </WrapperStyle>
+      <LeftMenu />
     </>
   );
 }
