4d8290ec1b4a3891887de4ce1edd3a0ab612d72c
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-05-16 11:53:37 +0300

    added btn settings

--- src/components/Webstatistics/Webstatistics.tsx
+++ src/components/Webstatistics/Webstatistics.tsx
@@ -1,41 +1,55 @@
 "use client";
 import React from "react";
 import { Icon } from "../common/Icon";
+import Gear from "@/assets/account/gear.png";
+import Image from "next/image";
 
 const Webstatistics = () => {
   return (
     <>
-      <div className="w-min">
+      <div className="w-max">
         <ul className="flex-col ml-20  text-gray-500 ">
           <li className="flex gap-3 mb-6 cursor-pointer  hover:text-green-900  ">
-            {" "}
-            <Icon name="mainhome" />{" "}
-            <p className="hover:text-green-900 ">Головна</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="mainhome" />{" "}
+              <p className="hover:text-green-900 ">Головна</p>
+            </button>
           </li>
           <li className="flex gap-3 mb-6 cursor-pointer hover:text-green-900  ">
-            {" "}
-            <Icon name="group" />
-            <p className="hover:text-green-900">Користувачі</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="group" />
+              <p className="hover:text-green-900">Користувачі</p>
+            </button>
           </li>
           <li className="flex gap-3 mb-6 cursor-pointer hover:text-green-900  ">
-            {" "}
-            <Icon name="menubook" />
-            <p className="hover:text-green-900">Книги</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="menubook" />
+              <p className="hover:text-green-900">Книги</p>
+            </button>
           </li>
           <li className="flex gap-3 mb-6 cursor-pointer hover:text-green-900  ">
-            {" "}
-            <Icon name="album" />
-            <p className="hover:text-green-900">Набори</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="album" />
+              <p className="hover:text-green-900">Набори</p>
+            </button>
           </li>
           <li className="flex gap-3 mb-6 cursor-pointer hover:text-green-900  ">
-            {" "}
-            <Icon name="sales" />
-            <p className="hover:text-green-900 ">Продажі</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="sales" />
+              <p className="hover:text-green-900 ">Продажі</p>
+            </button>
           </li>
           <li className="flex gap-3 mb-6 cursor-pointer hover:text-green-900 ">
-            {" "}
-            <Icon name="localization" />
-            <p className="hover:text-green-900">Локалізація</p>
+            <button className="flex gap-2 justify-center items-center ">
+              <Icon name="localization" />
+              <p className="hover:text-green-900">Локалізація</p>
+            </button>
+          </li>
+          <li className="flex   cursor-pointer hover:text-green-900 animate-pulse">
+            <button className="flex gap-2 justify-center items-center ">
+              <Image src={Gear} alt="Settings" className="w-6 h-6" />
+              <p>Оновити даннi</p>
+            </button>
           </li>
         </ul>
       </div>
