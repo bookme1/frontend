7e9dac1b3ef7536ac0c79a2442499afaa94c9a39
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2024-07-06 23:01:51 +0300

    created modal with settings

--- src/components/Modaladdbook/Modaladdbook.tsx
+++ src/components/Modaladdbook/Modaladdbook.tsx
@@ -17,7 +17,11 @@ export default function ModalAddBook({ isVisible, onClose }) {
   }, []);
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+      className="  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;"
       id="wrapper"
       onClick={handleClose}
     >
