6d3508cbaedd79fb619722aca58f63536f799029
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2024-07-07 18:00:13 +0300

    Загнал открытие/закрытие модалок в Редакс
    Пример использования:
    
      const dispatch = useDispatch();
      const modals = useSelector((state: RootState) => state.books.modals);
    
      const handleOpenModal = (modalName: string) => {
        dispatch(openModal(modalName));
      };
    
      const handleCloseModal = (modalName: string) => {
        dispatch(closeModal(modalName));
      };

--- src/lib/redux/rootReducer.ts
+++ src/lib/redux/rootReducer.ts
@@ -2,10 +2,11 @@
 import { adminApi } from './features/admin/adminApi';
 import { bookApi } from './features/book/bookApi';
 import { userApi } from './features/user/userApi';
-import { booksSlice } from './slices';
+import { modalsSlice } from './slices/modalsSlice';
+
 
 export const reducer = {
-  book: booksSlice.reducer,
+  book: modalsSlice.reducer,
   [userApi.reducerPath]: userApi.reducer,
   [bookApi.reducerPath]: bookApi.reducer,
   [adminApi.reducerPath]: adminApi.reducer,
