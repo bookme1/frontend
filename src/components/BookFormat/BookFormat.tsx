4a8855ab26bcc4f3e728aa6765eb36534703a472
Mykhailo <74735041+mikhail2574@users.noreply.github.com>
2024-07-08 21:46:53 +0200

    Add files via upload

---
+++ src/components/BookFormat/BookFormat.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { FormatsContainer } from './BookFormat.styles';
+
+import { Icon } from '../common/Icon';
+
+const BookFormat = ({ size }: { size: number }) => {
+  const renderBookFormatImage = (format: any) => {
+    let imagePath;
+    switch (format.toLowerCase()) {
+      case 'epub':
+        imagePath = 'epub';
+        break;
+      case 'mobi':
+        imagePath = 'mobi';
+        break;
+      case 'pdf':
+        imagePath = 'pdf';
+        break;
+      default:
+        imagePath = 'default.png';
+    }
+    return (
+      <Icon name={imagePath} data-format={format.toUpperCase()} size={size} />
+    );
+  };
+  const bookFormats = ['EPUB', 'MOBI', 'PDF'];
+  const renderedImages = bookFormats.map(format =>
+    renderBookFormatImage(format)
+  );
+  return (
+    <FormatsContainer className="formats-hover">
+      {renderedImages}
+    </FormatsContainer>
+  );
+};
+
+export default BookFormat;
