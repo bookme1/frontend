9d2a6eb62d433cbf2bd712486abbeb0d0f1349e9
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-07-10 11:38:33 +0200

    ai modal adaptive styles done

--- src/components/Modals/ModalContent/AiContent/AiContent.jsx
+++ src/components/Modals/ModalContent/AiContent/AiContent.jsx
@@ -1,16 +1,26 @@
-import Image from 'next/image';
+import { useState } from 'react';
 
 import aiModal from './AiContent.module.css';
 import AiContentMessage from '@/components/Modals/ModalContent/AiContent/AiContentMessage';
 import { Icon } from '@/components/common/Icon';
 
 const AiContent = () => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(message);
+    setMessage('');
+  };
+
   return (
     <div className={aiModal.ai_content_wrapper}>
       <div className={aiModal['ai-content-header']}>
         <div>
-          <h2>Привіт</h2>
-          <h3>Як я можу тобі допомогти?</h3>
+          <h2 className={aiModal['ai-content-header_title']}>Привіт</h2>
+          <h3 className={aiModal['ai-content-header_subtitle']}>
+            Як я можу тобі допомогти?
+          </h3>
         </div>
         <Icon name="ai-robot" width={120} height={120} />
       </div>
@@ -25,9 +35,16 @@ const AiContent = () => {
           вибрати книгу.
         </AiContentMessage>
         <div className={aiModal['message-form-block']}>
-          <form className={aiModal['message-form']}>
-            <input type="text" placeholder="Повідомлення" />
-            <button type="submit">Надіслати</button>
+          <form className={aiModal['message-form']} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Повідомлення"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <button type="submit" className={aiModal['button-send-message']}>
+              <Icon name="ai-modal-send-message" width={24} height={24} />
+            </button>
           </form>
         </div>
       </div>
--- src/components/common/Icons/Icons.tsx
+++ src/components/common/Icons/Icons.tsx
@@ -1702,6 +1702,33 @@ const Icons = () => {
               />
             </g>
           </symbol>
+
+          <symbol
+            id="ai-modal-send-message"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <mask
+              id="mask0_3856_5206"
+              // style="mask-type:alpha"
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+            >
+              <rect width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_3856_5206)">
+              <path
+                d="M3 20V4L22 12L3 20ZM5 17L16.85 12L5 7V10.5L11 12L5 13.5V17Z"
+                fill="white"
+              />
+            </g>
+          </symbol>
         </defs>
       </svg>
     </div>
