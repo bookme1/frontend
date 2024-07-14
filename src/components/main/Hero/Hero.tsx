8b359ec1fe8914ff7fa8048b4b99d9be6f4176e1
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2024-07-14 17:16:48 +0200

    fixed links

--- src/components/main/Hero/Hero.tsx
+++ src/components/main/Hero/Hero.tsx
@@ -1,64 +1,68 @@
 import {
-  CatalogButton,
-  HeroDescription,
-  HeroSection,
-  HeroTitle,
-  HeroWrapper,
-  ImageContainer,
-  NavContainer,
-  NavIcon,
-  NavItem,
-  NavLink,
-  NavList,
+    CatalogButton,
+    HeroDescription,
+    HeroSection,
+    HeroTitle,
+    HeroWrapper,
+    ImageContainer,
+    NavContainer,
+    NavIcon,
+    NavItem,
+    NavLink,
+    NavList,
 } from './Hero.styles';
 
 const Hero = () => {
-  return (
-    <HeroSection>
-      <HeroWrapper>
-        <NavContainer className="navigation">
-          <NavList>
-            <NavItem>
-              <NavLink className="accent">Акції</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                Блог Bookme <NavIcon name="arrow_right" className="icon" />
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                Блог Bookme <NavIcon name="arrow_right" className="icon" />
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                Паперові <NavIcon name="arrow_right" className="icon" />
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                Комплекти <NavIcon name="arrow_right" className="icon" />
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                Фантастика
-                <NavIcon name="arrow_right" className="icon" />
-              </NavLink>
-            </NavItem>
-          </NavList>
-        </NavContainer>
-        <ImageContainer>
-          <HeroTitle>Українські книжки</HeroTitle>
-          <HeroDescription>
-            Книги, що змінюють світ, тепер доступні для всього світу!
-          </HeroDescription>
-          <CatalogButton>До каталогу</CatalogButton>
-        </ImageContainer>
-      </HeroWrapper>
-    </HeroSection>
-  );
+    return (
+        <HeroSection>
+            <HeroWrapper>
+                <NavContainer className="navigation">
+                    <NavList>
+                        <NavItem>
+                            <NavLink className="accent">Акції</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/books">
+                                Каталог книжок
+                                <NavIcon name="arrow_right" className="icon" />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/conditions-of-use">
+                                Умови використання
+                                <NavIcon name="arrow_right" className="icon" />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/books">
+                                Кримінальні історії
+                                <NavIcon name="arrow_right" className="icon" />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/books">
+                               Художня література <NavIcon name="arrow_right" className="icon" />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/books">
+                                Фантастика
+                                <NavIcon name="arrow_right" className="icon" />
+                            </NavLink>
+                        </NavItem>
+                    </NavList>
+                </NavContainer>
+                <ImageContainer>
+                    <HeroTitle>Українські книжки</HeroTitle>
+                    <HeroDescription>
+                        Книги, що змінюють світ, тепер доступні для всього
+                        світу!
+                    </HeroDescription>
+                    <CatalogButton href="/books">До каталогу</CatalogButton>
+                </ImageContainer>
+            </HeroWrapper>
+        </HeroSection>
+    );
 };
 
 export default Hero;
