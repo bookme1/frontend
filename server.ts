d83985d7d2d7741688b33ff14526aa6665b99c86
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2024-11-14 12:52:59 +0100

    update server props

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { parse } from 'url';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
+const hostname = dev ? 'localhost' : 'bookme.kyiv.ua';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
