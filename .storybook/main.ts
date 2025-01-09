4a6f3cb61c7bd5f94fdffcaf392d2ea2096e0178
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2025-01-09 22:04:41 +0100

    add initial storybook lib

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
+    addons: [
+        '@storybook/addon-onboarding',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/addon-interactions',
+        '@storybook/addon-mdx-gfm'
+    ],
+
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+
+    staticDirs: ['..\\public'],
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
+    }
+};
+export default config;
