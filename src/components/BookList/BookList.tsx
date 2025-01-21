e433ddc1e2b4489e0725a3ae4d2e75bdda3f3a9e
Hennadii Bublikov <portfoliodavydko@gmail.com>
2025-01-21 15:52:46 +0200

    fix skeletons and type of BookList function

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,27 +1,31 @@
-import styles from './BookList.module.css'
-import { IBook } from "@/app/book/[id]/page.types";
-import BookItem from "../book/Item/BookItem";
+import styles from './BookList.module.css';
+import { BooksData, IBook } from '@/app/book/[id]/page.types';
 
+import BookItem from '../book/Item/BookItem';
 
-const BookList = ({filterBooks, handleOpenModal}:any) => {
-  return (
-    <ul className={styles.products__list}>
-    {filterBooks &&
-        filterBooks.books.map(
-            (book: IBook) => {
-                return (
-                    <BookItem
-                        key={book.id}
-                        book={book}
-                        handleOpenModal={
-                            handleOpenModal
-                        }
-                    />
-                );
-            }
-        )}
-</ul>
-  )
+interface BookListProps {
+    filterBooks?: BooksData;
+    handleOpenModal: (modalName: string, event: React.MouseEvent) => void;
 }
 
-export default BookList
+const BookList: React.FC<BookListProps> = ({
+    filterBooks,
+    handleOpenModal,
+}) => {
+    return (
+        <ul className={styles.products__list}>
+            {filterBooks &&
+                filterBooks.books.map((book: IBook) => {
+                    return (
+                        <BookItem
+                            key={book.id}
+                            book={book}
+                            handleOpenModal={handleOpenModal}
+                        />
+                    );
+                })}
+        </ul>
+    );
+};
+
+export default BookList;
